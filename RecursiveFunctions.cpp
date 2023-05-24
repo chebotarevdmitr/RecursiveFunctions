0bfbf147497cb21a07dcb66dd1fe3a888720d760
chebotarevdmitr <127180093+chebotarevdmitr@users.noreply.github.com>
2023-05-24 21:41:22 +0300

    Добавьте файлы проекта.

---
+++ RecursiveFunctions.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // rekurciya proces pri kotorom funkciya vizivaet samu sebya //
+
+    cout << "Hello RecursiveFunction\n";
+
+
+
+    system("pause>0");
+}
