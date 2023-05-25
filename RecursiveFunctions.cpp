78c0324c4b51c491c61951a52954c48ee147f17f
chebotarevdmitr <127180093+chebotarevdmitr@users.noreply.github.com>
2023-05-25 22:44:59 +0300

    cycle for

--- RecursiveFunctions.cpp
+++ RecursiveFunctions.cpp
@@ -1,14 +1,15 @@
 ﻿#include <iostream>
 
 using namespace std;
-
+//Sum numbers between m - n 
 int main()
 {
-    // rekurciya proces pri kotorom funkciya vizivaet samu sebya //
-
-    cout << "Hello RecursiveFunction\n";
-
-
-
+    int m = 2,  n = 4;
+    int sum = 0;
+    for (int i =m; i<= n; i++)
+    {
+        sum += i;
+    }
+    cout << "Sum =" << sum;
     system("pause>0");
 }
