8da650f89c57473a7b105a297f8561926f0ad749
chebotarevdmitr <127180093+chebotarevdmitr@users.noreply.github.com>
2023-05-25 23:46:12 +0300

    Recursive Functions

--- RecursiveFunctions.cpp
+++ RecursiveFunctions.cpp
@@ -1,15 +1,25 @@
 ﻿#include <iostream>
-
 using namespace std;
+
+int recursive_sum(int m, int n) {//m = 2,n = 4
+    if (m == n)
+        return n;
+    return n + recursive_sum(m + 1, n);
+}
+
 //Sum numbers between m - n 
 int main()
 {
-    int m = 2,  n = 4;
+   /*int m = 2, n = 4;
     int sum = 0;
     for (int i =m; i<= n; i++)
     {
         sum += i;
     }
     cout << "Sum =" << sum;
+   */
+
+    int m = 1, n = 5;
+    cout << "Sum = " << recursive_sum(m, n); 
     system("pause>0");
 }
